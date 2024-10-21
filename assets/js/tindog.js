a684534a310b8cf91f1e2473f7d89ece9120a7d5
Christian Onyeukwu <christianonyeukwu@Christians-MacBook-Air.local>
2024-10-21 03:31:41 -0400

    added update

---
+++ assets/js/tindog.js
@@ -0,0 +1,5 @@
+const copyRigt = document.querySelector('.copy-right');
+const currentDate = new Date();
+
+copyRigt.innerHTML += `${currentDate.getFullYear()} © Copyright TinDog. All Right Reserved <br/> `;
+copyRigt.innerHTML += `Designed by <a href="http://127.0.0.1:5501/index.html">Christian Onyeukwu<a/>`;
